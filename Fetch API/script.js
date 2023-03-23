0644eaca3a7000f1acbd896e07e322ef73714df8
Afnan Qureshi <Afnan.qureshi.x1@gmail.com>
2023-03-23 14:09:17 +0500

    post and get methods implimented

--- Fetch API/script.js
+++ Fetch API/script.js
@@ -8,3 +8,39 @@ p.then((response) => {
 }).then((value2) => {
     console.log(value2)
 })
+
+const createToDo = async(todo) => {
+let options = {
+    method: "POST",
+    headers: { "Content-type": "application/json" },
+    body: JSON.stringify(todo),
+
+}
+
+let d = await fetch('https://jsonplaceholder.typicode.com/posts', options)
+let response = await d.json();
+return response
+}
+
+const getTodo = async (id) => {
+    let response=  await fetch('https://jsonplaceholder.typicode.com/posts/1' + id)
+    let r =response.json()
+    return r
+
+}
+
+const mainFunc = async()=> {
+    let todo = {
+        title: 'afnan',
+        body: 'bhai',
+        userId: 10,
+    }
+    let todoRes = await createToDo(todo)
+    console.log(todoRes) //todoRes = todo Response
+    console.log( await getTodo(5))
+}
+
+
+mainFunc()
+
+

14099eb924bd202494e73cc54f2abfdc3f86b3b3
Afnan Qureshi <Afnan.qureshi.x1@gmail.com>
2023-03-23 15:49:01 +0500

    constructor function implimented

--- Arrays/script2.js
+++ Arrays/script2.js
@@ -12,4 +12,21 @@ const copy = (obj) => {
   const obj2 = copy(obj1); // obj2 looks like obj1 now
   console.log(obj1);
   console.log(obj2);
+
+  function Obj () {
+    this.name = "max",
+    this.greet = function(){
+        console.log(`hello i am ${this.name}`);
+    }
+  }
   
+  Obj.prototype.greetGeneral = function () {
+    console.log("greetings");
+  }
+  var obj = new Obj();
+  obj.name = "afnan";
+  obj.greet();
+
+  var anotherobj = new Obj();
+  anotherobj.greet();
+  anotherobj.greetGeneral();

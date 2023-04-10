8f0f64b51be7a35bc676d1d143df990c38bbc351
Afnan Qureshi <Afnan.qureshi.x1@gmail.com>
2023-04-10 23:15:10 +0500

    Bitwise Operatos implimented

---
+++ Maps and Bitwise Operators/script2.js
@@ -0,0 +1,27 @@
+// Bitwise AND operator &
+const result1 = 5 & 3;
+console.log(result1); // Output: 1
+
+// Bitwise OR operator |
+const result2 = 5 | 3;
+console.log(result2); // Output: 7
+
+// Bitwise XOR operator ^
+const result3 = 5 ^ 3;
+console.log(result3); // Output: 6
+
+// Bitwise NOT operator ~
+const result4 = ~5;
+console.log(result4); // Output: -6
+
+// Bitwise left shift operator <<
+const result5 = 5 << 1;
+console.log(result5); // Output: 10
+
+// Bitwise right shift operator >>
+const result6 = 5 >> 1;
+console.log(result6); // Output: 2
+
+// Bitwise zero-fill right shift operator >>>
+const result7 = -5 >>> 1;
+console.log(result7); // Output: 2147483645
